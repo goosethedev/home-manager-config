c93ba625c6e1be65d9f9c7ea2e31e752866752c7
Gus Sánchez <goosethedev@proton.me>
2024-01-02 10:37:44 -0400

    Switched VSCodium for VSCode and disabled settings for manual files

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -3,20 +3,20 @@
 {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscodium;
+    package = pkgs.vscode;
     extensions = with pkgs.vscode-extensions; [
       # github.copilot
       catppuccin.catppuccin-vsc
-      vscodevim.vim
+      # vscodevim.vim
       esbenp.prettier-vscode
       pkief.material-icon-theme
       bbenoist.nix
     ];
-    userSettings = {
-    	"workbench.sideBar.location" = "right";
-      "workbench.colorTheme" = "Catppuccin Mocha";
-      "catppuccin.accentColor" = "teal";
-      "material-icon-theme.folders.color" = "#115989";
-     };
+    # userSettings = {
+    # 	"workbench.sideBar.location" = "right";
+    #   "workbench.colorTheme" = "Catppuccin Mocha";
+    #   "catppuccin.accentColor" = "teal";
+    #   "material-icon-theme.folders.color" = "#115989";
+    #  };
   };
 }
