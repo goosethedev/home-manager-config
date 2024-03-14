f83c217afa16ab53d700f82f292ed2610f56f1d2
Gus Sánchez <goosethedev@proton.me>
2024-03-14 05:02:40 -0400

    Updated search engines for Firefox

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -52,10 +52,22 @@
           definedAliases = [ "@nw" ];
         };
     	  "Home Manager Options" = {
-          urls = [{ template = "https://mipmip.github.io/home-manager-option-search/?query={searchTerms}"; }];
+          urls = [{ template = "https://home-manager-options.extranix.com/?query={searchTerms}"; }];
           iconUpdateURL = "https://mipmip.github.io/home-manager-option-search/images/favicon.png";
           updateInterval = 24 * 60 * 60 * 1000; # every day
           definedAliases = [ "@hm" ];
+        };
+        "GitHub" = {
+          urls = [{ template = "https://github.com/search?q={searchTerms}&type=repositories";}];
+          iconUpdateURL = "https://github.githubassets.com/favicons/favicon-dark.png";
+          updateInterval = 24 * 60 * 60 * 1000; # every day
+          definedAliases = [ "@gh" ];
+        };
+        "Arch Wiki" = {
+          urls = [{ template = "https://wiki.archlinux.org/index.php?title=Special:Search&profile=default&search={searchTerms}";}];
+          iconUpdateURL = "https://wiki.archlinux.org/favicon.ico";
+          updateInterval = 24 * 60 * 60 * 1000; # every day
+          definedAliases = [ "@aw" ];
         };
     	  "YouTube" = {
           urls = [{ template = "https://www.youtube.com/results?search_query={searchTerms}"; }];
