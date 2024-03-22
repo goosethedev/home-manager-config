1836e357329a911965b2937a99d6419ec6b74d6d
Gus Sánchez <goosethedev@proton.me>
2024-03-22 11:19:25 -0400

    Arch config for Darknite

---
+++ hosts/darknite-arch.nix
@@ -0,0 +1,69 @@
+
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [
+    # Main config
+    ./common.nix
+
+    ../modules/vscode.nix
+    ../modules/eww.nix
+    ../modules/kitty.nix
+    # ../modules/wezterm.nix
+    ../modules/pass.nix
+    ../modules/hyprland.nix
+    ../modules/dunst.nix
+    ../modules/fcitx5.nix
+    ../modules/rofi.nix
+    ../modules/xkb.nix
+  ];
+
+  # For non NixOS systems
+  targets.genericLinux.enable = true;
+
+  # Local packages
+  home.packages = with pkgs; [
+    bitwarden
+    discord
+    element-desktop
+    gammastep
+    grimblast
+    gthumb
+    lunatask
+    neovim
+    pcmanfm
+    
+    # For OpenGL programs e.g. kitty
+    nixgl.auto.nixGLDefault
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # Override Nix kitty package with bin pointing to Arch local package that uses nixGLNvidia
+    ".local/bin/kitty" = {
+      enable = true;
+      executable = true;
+      text = "${pkgs.nixgl.auto.nixGLDefault} ${pkgs.kitty}";
+      # TODO: Investigate how to do this
+      # text = "${pkgs.nixgl.auto.nixGLNvidia} /usr/bin/kitty";
+    };
+  };
+
+  # Set env vars
+  home.sessionVariables = {
+    # Hyprland variables
+    # NixOS sets them automatically, Arch doesn't
+    # "LIBVA_DRIVER_NAME" = "nvidia";
+    "XDG_SESSION_TYPE" = "wayland";
+    # env = GBM_BACKEND,nvidia-drm
+    # "__GLX_VENDOR_LIBRARY_NAME" = "nvidia";
+    # "WLR_NO_HARDWARE_CURSORS" = "1";
+  };
+
+  # Start Hyprland on TTY login
+  programs.zsh.profileExtra = ''
+    Hyprland
+  '';
+}
+
