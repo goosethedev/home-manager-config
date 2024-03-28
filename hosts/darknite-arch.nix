b9d683b326d92a8cb104b44efad774637eea7e31
Gus Sánchez <goosethedev@proton.me>
2024-03-28 01:17:35 -0400

    Added nixGL directly to Hyprland

--- hosts/darknite-arch.nix
+++ hosts/darknite-arch.nix
@@ -40,30 +40,16 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # Override Nix kitty package with bin pointing to Arch local package that uses nixGLNvidia
-    ".local/bin/kitty" = {
-      enable = true;
-      executable = true;
-      text = "${pkgs.nixgl.auto.nixGLDefault} ${pkgs.kitty}";
-      # TODO: Investigate how to do this
-      # text = "${pkgs.nixgl.auto.nixGLNvidia} /usr/bin/kitty";
-    };
   };
 
   # Set env vars
   home.sessionVariables = {
-    # Hyprland variables
-    # NixOS sets them automatically, Arch doesn't
-    # "LIBVA_DRIVER_NAME" = "nvidia";
     "XDG_SESSION_TYPE" = "wayland";
-    # env = GBM_BACKEND,nvidia-drm
-    # "__GLX_VENDOR_LIBRARY_NAME" = "nvidia";
-    # "WLR_NO_HARDWARE_CURSORS" = "1";
   };
 
   # Start Hyprland on TTY login
   programs.zsh.profileExtra = ''
-    Hyprland
+    nixGL Hyprland
   '';
 }
 
