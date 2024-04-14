82bd9dec0c15a0354ac85da122816a6d28c6c3e2
Gus Sánchez <goosethedev@proton.me>
2024-04-14 16:16:09 -0400

    VSCode settings updated

--- modules/vscode/default.nix
+++ modules/vscode/default.nix
@@ -1,16 +1,23 @@
-{ config, pkgs, ... }:
-
-{
+{pkgs, ...}: {
   programs.vscode = {
     enable = true;
     package = pkgs.vscode;
     extensions = with pkgs.vscode-extensions; [
-      # github.copilot
+      formulahendry.auto-rename-tag
       catppuccin.catppuccin-vsc
-      # vscodevim.vim
+      catppuccin.catppuccin-vsc-icons
+      vspacecode.vspacecode
+      vscodevim.vim
       esbenp.prettier-vscode
-      pkief.material-icon-theme
+      bradlc.vscode-tailwindcss
+      ms-azuretools.vscode-docker
       bbenoist.nix
+      # github.copilot  # Don't have sub
+
+      # Missing (install manually)
+      # steoates - Auto Import
+      # dsznajder - ES7+ React Snippets
+      # playwright - Playwright
     ];
     # userSettings = {
     # 	"workbench.sideBar.location" = "right";
